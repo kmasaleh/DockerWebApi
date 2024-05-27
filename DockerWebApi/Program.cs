685347acb5dbea9c97ea66fd68a81ddd52f059a5
ksaleh <saleh208@hotmail.com>
2024-05-27 18:33:00 +0300

    initial commit

---
+++ DockerWebApi/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/readLogs", (HttpContext httpContext) =>
+{
+    string filePath = "logs/logs.txt";
+    if (!File.Exists(filePath))
+    {
+        httpContext.Response.WriteAsync("Logs file dos not exist!");
+        httpContext.Response.StatusCode = 404;
+        return "";
+    }
+    var logs= File.ReadAllText(filePath);
+    return logs;
+
+});
+app.MapGet("/echo", (string message) =>
+{
+    Console.WriteLine(message);
+    string filePath = "logs/logs.txt";
+    if(!File.Exists(filePath))
+    {
+        Directory.CreateDirectory("logs");
+       using StreamWriter writer = File.CreateText(filePath);
+        writer.WriteLine(message);
+
+    }
+    else
+    {
+        using StreamWriter writer = File.AppendText(filePath);
+        writer.WriteLine(message);
+    }
+    return message;
+})
+.WithName("Echo")
+.WithOpenApi();
+
+app.Run();
+
---
+++ Dockerfile
@@ -0,0 +1,26 @@
+﻿# Build Stage
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 as base
+WORKDIR /app
+
+FROM  mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /source
+COPY . . 
+RUN dotnet restore "./DockerWebApi/DockerWebApi.csproj" --disable-parallel
+RUN dotnet publish  "./DockerWebApi/DockerWebApi.csproj" -c release -o /app --no-restore
+FROM base AS final
+COPY --from=build /app ./
+EXPOSE 5001
+
+#VOLUME ["/app/logs"]
+
+ENTRYPOINT ["dotnet","DockerWebApi.dll"]
+# Serve Stage 
+
+
+
+
+#to open shell and inspect what happened inside the container
+#ENTRYPOINT []
+#docker run --name dockapi -it dockapi bash 
+
+#docker run --name dockapi -it  -p 5001:5001 dockapi dotnet DockerWebApi.dll --urls="http://0.0.0.0:5001"
